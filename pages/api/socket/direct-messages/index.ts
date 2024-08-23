fa55eb2e17a28ad1f13ab1233a3dc314ceca589d
Mazen Abdalla <92177398+MazenAbdallaAbdelhadi@users.noreply.github.com>
2024-08-23 17:53:49 +0300

    feat: direct messages

---
+++ pages/api/socket/direct-messages/index.ts
@@ -0,0 +1,98 @@
+import { auth } from "@/auth";
+import { currentUser } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { NextApiResponseServerIo } from "@/types";
+import { NextApiRequest } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponseServerIo
+) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  try {
+    const session = await auth(req, res);
+
+    if (!session) {
+      return res.status(401).json({ error: "unAuthorized" });
+    }
+
+    const profile = session.user;
+    const { content, fileUrl } = req.body;
+    const { conversationId } = req.query;
+
+    if (!conversationId) {
+      return res.status(400).json({ error: "Missing conversation id" });
+    }
+
+    if (!content) {
+      return res.status(400).json({ error: "Missing content" });
+    }
+
+    const conversation = await db.conversation.findFirst({
+      where: {
+        id: conversationId as string,
+        OR: [
+          {
+            memberOne: {
+              profileId: profile.id,
+            },
+          },
+          { memberTwo: { profileId: profile.id } },
+        ],
+      },
+      include: {
+        memberOne: {
+          include: {
+            profile: true,
+          },
+        },
+        memberTwo: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+
+    if (!conversation) {
+      return res.status(404).json({ error: "Conversation not found" });
+    }
+
+    const member =
+      conversation.memberOne.profileId === profile.id
+        ? conversation.memberOne
+        : conversation.memberTwo;
+
+    if (!member) {
+      return res.status(404).json({ message: "Memeber not found" });
+    }
+
+    const message = await db.directMessage.create({
+      data: {
+        content,
+        fileUrl,
+        conversationId: conversationId as string,
+        memberId: member.id,
+      },
+      include: {
+        member: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+
+    const channelKay = `chat:${conversationId}:messages`;
+
+    res?.socket?.server?.io?.emit(channelKay, message);
+
+    return res.status(200).json(message);
+  } catch (error) {
+    console.log("[DIRECT_MESSAGES_POST]", error);
+    return res.status(500).json({ message: "Internal error" });
+  }
+}
